b3d6098bf0fa4a4c3f35cf44792755a631104b6a
Patrick-Gould <61802530+patrick-gould@users.noreply.github.com>
2026-01-19 12:51:51 -0500

    added big-num and ecdh programs to simple serial directory.

---
+++ firmware/mcu/simpleserial-glitch-tiny-ecdh/simpleserial-glitch-tiny-ecdh.c
@@ -0,0 +1,107 @@
+/**
+    Author: Patrick T. Gould,
+    The Ohio State University - Department of Computer Science and Engineering.
+
+    This file, adapted from tiny-ecdh, contains a simple and small version of ecdh.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    See: <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hal.h"
+#include "simpleserial.h"
+#include "ecdh.h"
+
+#define PASS_SUCCESS 1 // If a glitch successfully happened.
+#define PASS_FAILURE 0 // Normal or corrupted run.
+uint8_t glitch_result = (uint8_t) PASS_FAILURE; // Holds if super_secret_function was ever called. 
+
+
+
+/// @brief A blank function that may used as a halting symbol; A point in the code to show a fault has occurred. E.g., an instruction skip allowed unreachable code—like this function—to be executed.
+uint8_t __attribute__((noinline)) super_secret_function()
+{
+    glitch_result = PASS_SUCCESS;
+    return PASS_SUCCESS;
+}
+
+
+/// @brief A simple test driver: raises trigger, invokes code under test, lowers trigger, tests success.
+#if SS_VER == SS_VER_2_1
+uint8_t ecdh(uint8_t cmd, uint8_t scmd, uint8_t dlen, uint8_t *data)
+#else
+uint8_t password(void) // Alternate header used if using simple_serial.1.x
+#endif
+{
+    // Enables ADC counter. This is how we count clock cycles since the ADC samples 4 times each cycle—by default, anyway. Takes roughly 45 cycles of overhead on the ICE40 with a Neorv32 flashed.
+    trigger_high();
+
+    testRunner(0);
+
+    trigger_low(); // Disables ADC counter.
+    
+    simpleserial_put('r', 1, (uint8_t *)&glitch_result); // Communicate result with python.
+
+    return 0x0; // simpleserial_put(...) talks to the outside world; we have no need to return anything here.
+}
+
+/// @brief  Waits for signal from Chipwhisperer and invokes callback to test function.
+/// @return none. Program is an infinite loop.
+int main(void)
+{
+    // Neorv32 platform init is empty, no need to call it.
+    platform_init();
+    // The 3 functions below just call neorv32.h functions.
+    init_uart();
+    trigger_setup();
+    simpleserial_init();
+
+// Set callback function(s).
+#if SS_VER == SS_VER_2_1
+    simpleserial_addcmd(0x01, 5, ecdh);
+#else
+    simpleserial_addcmd('p', 5, ecdh);
+#endif
+
+    while (1)
+        // Looks for input coming in from Chipwhisperer simpleserial, calls callback function on input.
+        simpleserial_get();
+}
+
+// Code and comments below are from tiny-ecdh.
+/*
+  Diffie-Hellman key exchange (without HMAC) aka ECDH_anon in RFC4492
+
+  1. Alice picks a (secret) random natural number 'a', calculates P = a * G and sends P to Bob.
+     'a' is Alice's private key. 
+     'P' is Alice's public key.
+
+  2. Bob picks a (secret) random natural number 'b', calculates Q = b * G and sends Q to Alice.
+     'b' is Bob's private key.
+     'Q' is Bob's public key.
+
+  3. Alice calculates S = a * Q = a * (b * G).
+
+  4. Bob calculates T = b * P = b * (a * G).
+
+  .. which are the same two values since multiplication in the field is commutative and associative.
+
+  T = S = the new shared secret.
+
+
+  Pseudo-random number generator inspired / stolen from: http://burtleburtle.net/bob/rand/smallprng.html
+*/
+
+
